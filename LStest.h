2f9f3413231c5245d16e42eb83cee6106561bf06
Jeff McLellan <webjedi@gmail.com>
2014-06-13 11:03:35 -0400

    added test to class

---
+++ LStest.h
@@ -0,0 +1,13 @@
+//
+//  LStest.h
+//  Git Test iOS
+//
+//  Created by Jeff McLellan on 6/13/14.
+//  Copyright (c) 2014 Lucidsyntax. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LStest : NSObject
+
+@end
---
+++ LStest.m
@@ -0,0 +1,13 @@
+//
+//  LStest.m
+//  Git Test iOS
+//
+//  Created by Jeff McLellan on 6/13/14.
+//  Copyright (c) 2014 Lucidsyntax. All rights reserved.
+//
+
+#import "LStest.h"
+
+@implementation LStest
+
+@end
